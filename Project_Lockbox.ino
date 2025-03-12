1fa62f35db8a05aa8ea4a49e7b2317c69eecdc67
Luka Buchheim <lukabuchheim@lukas-mbp-2.milton.edu>
2025-03-12 11:25:54 -0400

    added LED's to EXPLODE

--- Project_Lockbox.ino
+++ Project_Lockbox.ino
@@ -4,24 +4,19 @@
 #define GREEN_LED 2
 #define SERVO_PIN 6
 
-//ultra sonic sensor
-#define TRIGGERPIN 3 // this pin is the pin that sends out pulse 
-#define ECHOPIN 2 //this pin reads the distance. 
-
-
+ #include <Servo.h>
+Servo TheServo; 
 
 
 
 void setup() {
    Serial.begin(9600); //so we can print if we want. 
- pinMode(TRIGGERPIN, OUTPUT); //send pulse 
- pinMode(ECHOPIN, INPUT); //reads pulse
  pinMode(YELLOW_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT); 
   pinMode(BUZZER_PIN, OUTPUT);
-   Servo.attach(SERVO_PIN); //connecting servo object to pin  
-  Servo.write(90); //set start of propeller to 90 degrees 
+   TheServo.attach(SERVO_PIN); //connecting servo object to pin  
+  TheServo.write(90); //set start of propeller to 90 degrees 
 
 }
 
@@ -29,29 +24,72 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 delay(300);
- Servo.write (360);
+  TheServo.write (360);
 delay(300); 
- Servo.write (390);
+ TheServo.write (390);
  delay(300); 
- Servo.write (450);
+ TheServo.write (450);
  delay(300); 
- Servo.write (480);
+ TheServo.write (480);
  delay(300); 
- Servo.write (520);
+ TheServo.write (520);
  delay(300); 
 
- digital.write(GREEN_LED, HIGH);
- digital.write(YELLOW_LED, HIGH);
+//LED FLASHING 
 
-   for (int i = 0; i < 7; i++) {
-      digitalWrite(RED_LED, HIGH);
-      delay(250);  // On for 0.25 seconds
-      digitalWrite(GREEN_LED, LOW);
-      delay(100);  // Off for 0.25 seconds
-      digitalWrite(RED_LED, LOW);
+for (int i = 0; i < 10; i++) {
+        digitalWrite(RED_LED, HIGH);
+        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(YELLOW_LED, HIGH);
+        delay(250);
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(GREEN_LED, HIGH);
+        digitalWrite(YELLOW_LED, LOW);
+        delay(250);
+        digitalWrite(GREEN_LED, LOW);  
+    delay(250);                   
+    }
 
+    //LED FLASHING
+    for (int i = 0; i < 7; i++) {
+        digitalWrite(RED_LED, HIGH);
+        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(YELLOW_LED, HIGH);
+        delay(250);
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(GREEN_LED, HIGH);
+        digitalWrite(YELLOW_LED, LOW);
+        delay(250);
+    }
 
+  //LED FLASHING
+    for (int i = 0; i < 7; i++) {
+        digitalWrite(RED_LED, HIGH);
+        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(YELLOW_LED, HIGH);
+        delay(100);
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(GREEN_LED, LOW);
+        digitalWrite(YELLOW_LED, LOW);
+        delay(100);
+    }
 
+  for (int i = 0; i < 10; i++) {
+        digitalWrite(RED_LED, HIGH);
+        delay(100);
+        digitalWrite(RED_LED, LOW); // Turn RED LED OFF
+        delay(100);
+    }
+
+   
+  
 
 
 }
+
+
+
+
+
+
+
