2bc064897e079e5e440c338938fb1627dcc29a04
Luka Buchheim <lukabuchheim@Lukas-MacBook-Pro-2.local>
2025-04-02 13:09:22 -0400

    added RFID code

--- Project_Lockbox.ino
+++ Project_Lockbox.ino
@@ -7,6 +7,28 @@
  #include <Servo.h>
 Servo TheServo; 
 
+#include <MFRC522.h>
+
+#include <MFRC522Extended.h>
+
+#include <deprecated.h>
+
+#include <require_cpp11.h>
+
+
+#include <SPI.h>
+
+#include <MFRC522.h>
+
+
+
+#define RST_PIN 9 // Configurable, see typical pin layout above
+
+#define SS_PIN 10 // Configurable, see typical pin layout above
+
+
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 
 void setup() {
@@ -17,6 +39,23 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
    TheServo.attach(SERVO_PIN); //connecting servo object to pin  
   TheServo.write(90); //set start of propeller to 90 degrees 
+
+  while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+
+SPI.begin(); // Init SPI bus
+
+mfrc522.PCD_Init(); // Init MFRC522
+
+delay(4); // Optional delay. Some board do need more time after init to be ready, see Readme
+
+mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+
+Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
 
 }
 
@@ -83,6 +122,57 @@ for (int i = 0; i < 10; i++) {
 
    
   
+if ( ! mfrc522.PICC_IsNewCardPresent()) {
+
+return;
+
+}
+
+
+
+ // Select one of the cards
+
+if ( ! mfrc522.PICC_ReadCardSerial()) {
+
+return;
+
+}
+
+
+
+
+MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+
+
+
+Serial.print(F("RFID Tag UID:"));
+
+printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
+
+Serial.println("");
+
+
+
+mfrc522.PICC_HaltA(); // Halt PICC
+
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+
+
+
+ //Serial.begin("reading?");
+
+for (byte i = 0; i < bufferSize; i++) {
+
+Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+
+Serial.print(buffer[i], HEX);
+
+}
+
+
+if()
 
 
 }
