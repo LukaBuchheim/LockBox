76fc7769bc3148db2416e184e46fbfd5ef616cca
Luka Buchheim <lukabuchheim@Lukas-MacBook-Pro-3.local>
2025-04-18 08:44:42 -0400

    a few errors i need to fix, but I added bluetooth and took away RFID code because i need to fifigure it out

--- Project_Lockbox.ino
+++ Project_Lockbox.ino
@@ -7,23 +7,19 @@
 #define TRIGGERPIN 7 // this pin is the pin that sends out pulse 
 #define ECHOPIN 8 //this pin reads the distance. 
 
- #include <Servo.h>
-Servo TheServo; 
+int const RX_PIN = A2; //not using define because bluetooth doesn't like it. This is RX Pin  //receiving signal
+int const TX_PIN = A4; //sends signal 
+#include <SoftwareSerial.h> 
+// My bluetooth ends in AB24C5
 
-#include <MFRC522.h>
-
-#include <MFRC522Extended.h>
-
-#include <deprecated.h>
-
-#include <require_cpp11.h>
-
+#include <Servo.h>
+#define MICHAEL_PIN 9
 
 #include <SPI.h>
 
 #include <MFRC522.h>
 
-
+Servo TheServo; 
 
 #define RST_PIN 9 // Configurable, see typical pin layout above
 
@@ -32,6 +28,11 @@ Servo TheServo;
 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+SoftwareSerial tooth(TX_PIN, RX_PIN); //make a bluetooth object 
+//set tx and rx pins 
+//tx goes first, then rx 
+char davis; 
 
 
 void setup() {
@@ -57,6 +58,8 @@ Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 
 pinMode(TRIGGERPIN, OUTPUT); //send pulse 
  pinMode(ECHOPIN, INPUT); //reads pulse
+
+ tooth.begin(9600); //starts my bluetooth
 
 
 }
@@ -139,13 +142,10 @@ delay(100);
         digitalWrite(RED_LED, HIGH);
         delay(100);
         digitalWrite(RED_LED, LOW); // Turn RED LED OFF
-        delay(100);
-
-        TheServo.write (390);
         delay(300); 
 
 
- 
+
     }
 tone(BUZZER_PIN, 1000); 
 delay(1000); 
@@ -153,88 +153,45 @@ noTone(BUZZER_PIN);
 delay(500);   
 
   } 
-   if (0 < distance < 6) {
-   for (int i = 0; i < 10; i++) {
-        digitalWrite(GREEN_LED, HIGH);
-        digitalWrite(YELLOW_LED, LOW);
-        digitalWrite(RED_LED,LOW ); // Turn RED LED OFF
-        delay(100);
-    
+
+   if ((0 < distance < 6) {
+     for (int i = 0; i < 8; i++) {
+      digitalWrite(GREEN_LED, HIGH);
+      digitalWrite(YELLOW_LED, LOW);
+      digitalWrite(RED_LED, LOW); 
+      delay(100);
+
+      if (tooth.available() > 0) {
+        davis = tooth.read();
+        tooth.print("Input: ");
+        tooth.println(davis);
+      }
+
+      if (davis == "Platypus") {
+        Serial.println("Access Granted!"); 
+        delay(300);
+        TheServo.write(360);
+        delay(300); 
+        TheServo.write(390);
+        delay(300); 
+        TheServo.write(450);
+        delay(300); 
+        TheServo.write(480);
+        delay(300); 
+        TheServo.write(520);
+        delay(1000); 
+        TheServo.write(450);
+        delay(300);
+        TheServo.write(420);
+        delay(300); 
+        TheServo.write(390);
+        delay(300); 
+      } 
     } 
-  } 
-
-// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-
-if ( ! mfrc522.PICC_IsNewCardPresent()) {
-
-return;
-
-}
+  }
 
 
 
- // Select one of the cards
-
-if ( ! mfrc522.PICC_ReadCardSerial()) {
-
-return;
-
-}
-
-
-
-
-MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-
-
-
-Serial.print(F("RFID Tag UID:"));
-
-printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
-
-Serial.println("");
-
-
-
-mfrc522.PICC_HaltA(); // Halt PICC
-
-}
-
-void printHex(byte *buffer, byte bufferSize) {
-
-
-
- //Serial.begin("reading?");
-
-for (byte i = 0; i < bufferSize; i++) {
-
-Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-
-Serial.print(buffer[i], HEX);
-
-}
-     
-
-
-delay(300);
-  TheServo.write (360);
-delay(300); 
- TheServo.write (390);
- delay(300); 
- TheServo.write (450);
- delay(300); 
- TheServo.write (480);
- delay(300); 
- TheServo.write (520);
- delay(1000); 
- TheServo.write (450);
-delay(300);
-TheServo.write (420);
-delay(300); 
- TheServo.write (390);
- delay(300); 
-
-}
 
 
 
